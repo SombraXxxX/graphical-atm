49762e4b9e1594a7cd9c119de471f8441f8fde9f
SombraXxxX <43217593+SombraXxxX@users.noreply.github.com>
2019-01-11 22:33:40 -0500

    Update HomeView.java

--- src/view/HomeView.java
+++ src/view/HomeView.java
@@ -14,6 +14,16 @@ public class HomeView extends JPanel implements ActionListener {
 	
 	private ViewManager manager;		// manages interactions between the views, model, and database
 	
+	private JLabel AccountMessage;
+	private JButton DepositButton;
+	private JButton WithdrawButton;
+	private JButton TransferButton;
+	private JButton EditInfoButton;
+	private JButton CloseAccountButton;
+	private JButton LogoutButton;
+
+
+
 	/**
 	 * Constructs an instance (or objects) of the HomeView class.
 	 * 
@@ -34,13 +44,16 @@ public class HomeView extends JPanel implements ActionListener {
 	 */
 	
 	private void initialize() {
-		
+		initAccountMessage();
+		initLogoutButton();
+		initDepositButton();
+		initWithdrawButton();
+		initTransferButton();
 		// TODO
 		//
 		// this is a placeholder for this view and should be removed once you start
 		// building the HomeView.
 		
-		this.add(new javax.swing.JLabel("HomeView", javax.swing.SwingConstants.CENTER));
 		
 		// TODO
 		//
@@ -51,6 +64,37 @@ public class HomeView extends JPanel implements ActionListener {
 		// positioning your components.
 	}
 	
+	private void initAccountMessage() {
+		AccountMessage = new JLabel("", SwingConstants.CENTER);
+		AccountMessage.setBounds(50,50,500,50);
+		this.add(accountMessage);
+	}
+	
+	private void initWithdrawButton () {
+		WithdrawButton = new JButton("Withdraw");
+		WithdrawButton.setBounds(200,100,100,100);
+		WithdrawButton.addActionListener(this);
+		this.add(WithdrawButton);
+	}
+	
+	private void initDepositButton() {
+		DepositButton = new JButton("Deposit");
+		DepositButton.setBounds(50,100,100,100);
+	}
+	
+	private void initTransferButton() {
+		TransferButton = new JButton("Transfer");
+		TransferButton.setBounds(50,250,100,100);
+		TransferButton.addActionListener(this);
+		this.add(TransferButton);	
+	}
+	
+	private void initLogoutButton() {
+		LogoutButton = new JButton("Log out");
+		LogoutButton.setBounds(5,5,100,50);
+		LogoutButton.addActionListen(this);
+		this.add(LogoutButton);
+	}
 	/*
 	 * HomeView is not designed to be serialized, and attempts to serialize will throw an IOException.
 	 * 
