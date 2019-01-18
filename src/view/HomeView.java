2aae83f2a15cafe73a0b6635c896400f7b3c549e
SombraXxxX <43217593+SombraXxxX@users.noreply.github.com>
2019-01-18 11:54:45 -0500

    Update HomeView.java

--- src/view/HomeView.java
+++ src/view/HomeView.java
@@ -1,36 +1,49 @@
 package view;
 
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.text.ParseException;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.text.MaskFormatter;
+
+import com.toedter.calendar.JDateChooser;
+
+import javax.swing.JPasswordField;
+import javax.swing.JComboBox;
+import javax.swing.JFormattedTextField;
+import javax.swing.JRadioButton;
 
 import controller.ViewManager;
 
-@SuppressWarnings("serial")
-public class HomeView extends JPanel implements ActionListener {
+public class CreateView extends JPanel implements ActionListener {
 	
 	private ViewManager manager;		// manages interactions between the views, model, and database
-	
-	private JLabel AccountMessage;
-	private JButton DepositButton;
-	private JButton WithdrawButton;
-	private JButton TransferButton;
-	private JButton EditInfoButton;
-	private JButton CloseAccountButton;
-	private JButton LogoutButton;
-
-
-
+	private JTextField fName;	// first name
+	private JTextField lName;	// last name
+	private JTextField Address;
+	private JTextField City;
+	private JComboBox State;
+	private JDateChooser dob;
+	private JTextField PhoneNum;
+	private JTextField Zipcode;
+	private JButton CreateButton;
+	private JButton returnButton;
+	private JPasswordField Pin;
 	/**
-	 * Constructs an instance (or objects) of the HomeView class.
+	 * Constructs an instance (or object) of the CreateView class.
 	 * 
 	 * @param manager
 	 */
 	
-	public HomeView(ViewManager manager) {
+	public CreateView(ViewManager manager) {
 		super();
 		
 		this.manager = manager;
@@ -40,78 +53,198 @@ public class HomeView extends JPanel implements ActionListener {
 	///////////////////// PRIVATE METHODS /////////////////////////////////////////////
 	
 	/*
-	 * Initializes the HomeView components.
+	 * Initializes the CreateView components.
 	 */
 	
 	private void initialize() {
-		initAccountMessage();
-		initLogoutButton();
-		initDepositButton();
-		initWithdrawButton();
-		initTransferButton();
+		this.setLayout(null);
+		
+		initfName();
+		initlName();
+		initPhoneNum();
+		initAddress();
+		initCity();
+		initZipcode();
+		initCreateButton();
+		initreturnButton();
+		initdob();
+		initState();
+		initPin();
+		
 		// TODO
 		//
-		// this is a placeholder for this view and should be removed once you start
-		// building the HomeView.
-		
-		
-		// TODO
-		//
-		// this is where you should build the HomeView (i.e., all the components that
-		// allow the user to interact with the ATM - deposit, withdraw, transfer, etc.).
+		// this is where you should build the CreateView (i.e., all the components that
+		// allow the user to enter his or her information and create a new account).
 		//
 		// feel free to use my layout in LoginView as an example for laying out and
 		// positioning your components.
 	}
 	
-	private void initAccountMessage() {
-		AccountMessage = new JLabel("", SwingConstants.CENTER);
-		AccountMessage.setBounds(50,50,500,50);
-		this.add(AccountMessage);
+	private void initreturnButton() {
+		returnButton = new JButton("Return");
+		returnButton.setBounds(5, 5, 75, 50);
+		returnButton.addActionListener(this);
+
+		
+		this.add(returnButton);
+		
+	}
+
+	private void initCreateButton() {
+		// TODO Auto-generated method stub
+		CreateButton = new JButton("Create Account");
+		CreateButton.setBounds(175, 420, 200, 35);
+		CreateButton.addActionListener(this);
+		
+		this.add(CreateButton);
+	}
+
+	private void initPhoneNum() {
+		JLabel label = new JLabel("Phone Number:", SwingConstants.RIGHT);
+		label.setBounds(70, 125, 125, 35);
+		
+	    try {
+			MaskFormatter phoneFormat = new MaskFormatter("(###)-###-####");
+			phoneFormat.setPlaceholderCharacter('_');
+			PhoneNum = new JFormattedTextField(phoneFormat);
+		} catch (ParseException e) {
+			PhoneNum.setText("");
+		}
+		PhoneNum.setBounds(205, 125, 100, 35);
+		this.add(label);
+		this.add(PhoneNum);
+
+
+	}
+
+	private void initlName() {
+		JLabel label = new JLabel("Last Name:", SwingConstants.RIGHT);
+		label.setBounds(70,80,125,35);
+		label.setLabelFor(lName);
+		label.setFont(new Font("DialogInput", Font.BOLD, 14));
+		
+		lName = new JTextField(20);
+		lName.setBounds(205, 80, 200, 35);
+		
+		this.add(label);
+		this.add(lName);
+		
+	}
+
+	private void initfName() {
+		JLabel label = new JLabel("First Name:", SwingConstants.RIGHT);
+		label.setBounds(70,30,125,35);
+		label.setLabelFor(lName);
+		label.setFont(new Font("DialogInput", Font.BOLD,  14));
+		
+		lName = new JTextField(20);
+		lName.setBounds(205, 30, 200, 35);
+		
+		this.add(label);
+		this.add(lName);// TODO Auto-generated method stub
+		
+	}
+	private void initAddress() {
+		JLabel label = new JLabel("Street Address:", SwingConstants.RIGHT);
+		label.setBounds(70,165,125,35);
+		label.setLabelFor(Address);
+		label.setFont(new Font("DialogInput", Font.BOLD, 14));
+		
+		Address = new JTextField(20);
+		Address.setBounds(205, 165, 200, 35);
+		
+		
+		this.add(label);
+		this.add(Address);// TODO Auto-generated method stub
+		
+	}
+	private void initCity() {
+		JLabel label = new JLabel("City:", SwingConstants.RIGHT);
+		label.setBounds(70,205,125,35);
+		label.setLabelFor(City);
+		label.setFont(new Font("DialogInput", Font.BOLD, 14));
+		
+		City = new JTextField(20);
+		City.setBounds(205, 205, 200, 35);
+		
+		
+		this.add(label);
+		this.add(City);// TODO Auto-generated method stub
+		
+	}
+	private void initZipcode() {
+		JLabel label = new JLabel("Zipcode:", SwingConstants.RIGHT);
+		label.setBounds(70,245,125,35);
+		label.setLabelFor(Zipcode);
+		label.setFont(new Font("DialogInput", Font.BOLD, 14));
+		
+		Zipcode = new JTextField(20);
+		Zipcode.setBounds(205, 245, 200, 35);
+		
+		
+		this.add(label);
+		this.add(Zipcode);// TODO Auto-generated method stub
+		
 	}
 	
-	private void initWithdrawButton () {
-		WithdrawButton = new JButton("Withdraw");
-		WithdrawButton.setBounds(200,100,100,100);
-		WithdrawButton.addActionListener(this);
-		this.add(WithdrawButton);
+	private void initState() {
+		JLabel label = new JLabel("State", SwingConstants.RIGHT);
+		label.setBounds(70, 285, 95, 35);
+		label.setLabelFor(State);
+		label.setFont(new Font("DialogInput", Font.BOLD, 14));
+
+		String[] listStates = {"AK","AL","AR","AZ","CA","CO","CT","DC","DE",
+				"FL","GA","GU","HI","IA","ID", "IL","IN","KS","KY","LA","MA",
+				"MD","ME","MH","MI","MN","MO","MS","MT","NC","ND","NE","NH",
+				"NJ","NM","NV","NY", "OH","OK","OR","PA","PR","PW","RI","SC",
+				"SD","TN","TX","UT","VA","VI","VT","WA","WI","WV","WY"};
+
+		
+		State = new JComboBox(listStates);
+		State.setBounds(205, 285, 200, 35);
+		
+		this.add(label);
+		this.add(State);
 	}
 	
-	private void initDepositButton() {
-		DepositButton = new JButton("Deposit");
-		DepositButton.setBounds(50,100,100,100);
-		DepositButton.addActionListen(this);
-		this.add(DepositButton);
+	private void initdob() {
+		JLabel label = new JLabel("DOB: ", SwingConstants.RIGHT);
+		label.setBounds(70, 365, 95, 35);
+		label.setLabelFor(dob);
+		label.setFont(new Font("DialogInput", Font.BOLD, 14));
+		
+		dob = new JDateChooser();
+		dob.setBounds(205, 365, 200, 35);
+		
+		this.add(label);
+		this.add(dob);
 	}
-	
-	private void initTransferButton() {
-		TransferButton = new JButton("Transfer");
-		TransferButton.setBounds(50,250,100,100);
-		TransferButton.addActionListener(this);
-		this.add(TransferButton);	
-	}
-	
-	private void initLogoutButton() {
-		LogoutButton = new JButton("Log out");
-		LogoutButton.setBounds(5,5,100,50);
-		LogoutButton.addActionListen(this);
-		this.add(LogoutButton);
+	private void initPin() {
+		JLabel label = new JLabel("Pin:", SwingConstants.RIGHT);
+		label.setBounds(70, 325, 95, 35);
+		label.setLabelFor(Pin);
+		label.setFont(new Font("DialogInput", Font.BOLD, 14));
+		Pin = new JPasswordField(20);
+		Pin.setBounds(205, 325, 200, 35);
+		
+		this.add(label);
+		this.add(Pin);
 	}
 	/*
-	 * HomeView is not designed to be serialized, and attempts to serialize will throw an IOException.
+	 * CreateView is not designed to be serialized, and attempts to serialize will throw an IOException.
 	 * 
-	 * @param oos
+	 * @param 
 	 * @throws IOException
 	 */
 	
 	private void writeObject(ObjectOutputStream oos) throws IOException {
-		throw new IOException("ERROR: The HomeView class is not serializable.");
+		throw new IOException("ERROR: The CreateView class is not serializable.");
 	}
 	
 	///////////////////// OVERRIDDEN METHODS //////////////////////////////////////////
 	
 	/*
-	 * Responds to button clicks and other actions performed in the HomeView.
+	 * Responds to button clicks and other actions performed in the CreateView.
 	 * 
 	 * @param e
 	 */
@@ -119,17 +252,13 @@ public class HomeView extends JPanel implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		Object source = e.getSource();
-		if (source.equals(LogoutButton)) {
-			manager.shutdown();
-		} else if (source.equals(DepositButton)) {
-			manager.switchTo(ATM.DESPOSIT_VIEW);
-		} else if (source.equals(WithdrawButton)) {
-			manager.switchTo(ATM.WITHDRAW_VIEW);
-		} else if (source.equals(TransferButton)) {
-			manager.switchTo(ATM.TRANSFER_VIEW);
-		} else {
+		if (source.equals(CreateButton)) {
+			
+			manager.switchTo(ATM.HOME_VIEW);
+	} else if (source.equals(returnButton)) {
+			manager.switchTo(ATM.LOGIN_VIEW);
+	} else {
 		System.err.println("ERROR: Action command not found (" + e.getActionCommand() + ")");
-		// TODO
 		//
 		// this is where you'll setup your action listener, which is responsible for
 		// responding to actions the user might take in this view (an action can be a
@@ -137,7 +266,5 @@ public class HomeView extends JPanel implements ActionListener {
 		//
 		// feel free to use my action listener in LoginView.java as an example.
 	}
-	public void AccountMessage() {
-		AccountMessage.setText(manager.getAccount().getUser().getFirstName() + " " + manager.getAccount().getUser().getLastName() + "; " + manager.getAccount().getAccountNumber() + "; " + manager.getAccount().getBalance());
 	}
 }
